0989b221103ef553a491efeb89e91299c394368d
David Jackson <david@dwjackson.ca>
2016-06-10 23:12:52 -0400

    Add functions for stack manipulation in parser

--- lib/parser.c
+++ lib/parser.c
@@ -34,6 +34,34 @@ parse_table_check_match(enum ctache_token_type non_terminal,
     return match;
 }
 
+static void
+push_token_onto_stack(struct linked_list *stack,
+                      enum ctache_token_type token_type)
+{
+    enum ctache_token_type *token_type_ptr;
+    token_type_ptr = malloc(sizeof(*token_type_ptr));
+    *token_type_ptr = token_type;
+    linked_list_push(stack, token_type_ptr);
+}
+
+static void
+pop_token_off_stack(struct linked_list *stack)
+{
+    void *ptr;
+    ptr = linked_list_pop(stack);
+    free(ptr);
+    ptr = NULL;
+}
+
+static void
+append_parsed_rule(struct linked_list *parsed_rules, int rule_index)
+{
+    int *rule_ptr;
+    rule_ptr = malloc(sizeof(*rule_ptr));
+    *rule_ptr = rule_index;
+    linked_list_append(parsed_rules, rule_ptr);
+}
+
 struct linked_list
 *ctache_parse(struct linked_list *tokens)
 {
@@ -42,20 +70,14 @@ struct linked_list
     struct ctache_token *token;
     enum ctache_token_type *token_type_ptr;
     int match;
-    void *ptr;
     int *rule_ptr;
     struct linked_list *stack;
 
     /* Set up the stack */
     stack = linked_list_create();
 
-    token_type_ptr = malloc(sizeof(*token_type_ptr));
-    *token_type_ptr = CTACHE_TOK_EOI;
-    linked_list_push(stack, token_type_ptr);
-
-    token_type_ptr = malloc(sizeof(*token_type_ptr));
-    *token_type_ptr = CTACHE_TOK_TEMPLATE;
-    linked_list_push(stack, token_type_ptr);
+    push_token_onto_stack(stack, CTACHE_TOK_EOI);
+    push_token_onto_stack(stack, CTACHE_TOK_TEMPLATE);
 
     /* Set up the parsed rule list */
     parsed_rules = linked_list_create();
@@ -67,73 +89,45 @@ struct linked_list
         token_type_ptr = linked_list_peek(stack);
         match = parse_table_check_match(*token_type_ptr, token->tok_type);
         if (*token_type_ptr == token->tok_type) {
-            ptr = linked_list_pop(stack);
-            free(ptr);
-            ptr = NULL;
+            pop_token_off_stack(stack);
             token_node = token_node->next;
         } else if (match >= 0) {
-            ptr = linked_list_pop(stack);
-            free(ptr);
-            ptr = NULL;
+            pop_token_off_stack(stack);
 
             switch (match) {
             case 0: /* Rule 1: template -> text, template */
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_TEMPLATE;
-                linked_list_push(stack, token_type_ptr);
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_TEXT;
-                linked_list_push(stack, token_type_ptr);
+                push_token_onto_stack(stack, CTACHE_TOK_TEMPLATE);
+                push_token_onto_stack(stack, CTACHE_TOK_TEXT);
                 break;
             case 1: /* Rule 2: template -> tag, template */
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_TEMPLATE;
-                linked_list_push(stack, token_type_ptr);
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_TAG;
-                linked_list_push(stack, token_type_ptr);
+                push_token_onto_stack(stack, CTACHE_TOK_TEMPLATE);
+                push_token_onto_stack(stack, CTACHE_TOK_TAG);
                 break;
             case 2: /* Rule 3: template -> "" */
                 break;
             case 3: /* Rule 4: text -> string */
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_STRING;
-                linked_list_push(stack, token_type_ptr);
+                push_token_onto_stack(stack, CTACHE_TOK_STRING);
                 break;
             case 4: /* Rule 5: text -> "" */
                 break;
             case 5: /* Rule 6: tag -> tag start, string, tag end */
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_TAG_END;
-                linked_list_push(stack, token_type_ptr);
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_STRING;
-                linked_list_push(stack, token_type_ptr);
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_TAG_START;
-                linked_list_push(stack, token_type_ptr);
+                push_token_onto_stack(stack, CTACHE_TOK_TAG_END);
+                push_token_onto_stack(stack, CTACHE_TOK_STRING);
+                push_token_onto_stack(stack, CTACHE_TOK_TAG_START);
                 break;
             case 6: /* Rule 7: tag start -> section tag start */
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_SECTION_TAG_START;
-                linked_list_push(stack, token_type_ptr);
+                push_token_onto_stack(stack, CTACHE_TOK_SECTION_TAG_START);
                 break;
             case 7: /* Rule 8: tag start -> close tag start */
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_CLOSE_TAG_START;
-                linked_list_push(stack, token_type_ptr);
+                push_token_onto_stack(stack, CTACHE_TOK_CLOSE_TAG_START);
                 break;
             case 8: /* Rule 9: tag start -> value tag start */
-                token_type_ptr = malloc(sizeof(*token_type_ptr));
-                *token_type_ptr = CTACHE_TOK_VALUE_TAG_START;
-                linked_list_push(stack, token_type_ptr);
+                push_token_onto_stack(stack, CTACHE_TOK_VALUE_TAG_START);
                 break;
             default:
                 break;
             }
-            rule_ptr = malloc(sizeof(*rule_ptr));
-            *rule_ptr = match;
-            linked_list_append(parsed_rules, rule_ptr);
+            append_parsed_rule(parsed_rules, match);
         } else {
             fprintf(stderr, "Syntax error\n");
             linked_list_destroy(parsed_rules);
