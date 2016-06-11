fde7a7185333a408c721564395be7ef240365e9a
David Jackson <david@dwjackson.ca>
2016-06-10 21:34:56 -0400

    Fix a format string typo

--- src/main.c
+++ src/main.c
@@ -124,7 +124,7 @@ main(int argc, char *argv[])
 
     parsed_rules = ctache_parse(tokens);
     if (print_parsed_rules) {
-        printf("Parsed rules: ]n");
+        printf("Parsed rules:\n");
         for (curr = parsed_rules->first; curr != NULL; curr = curr->next) {
             int *rule_ptr = curr->data;
             printf("\t%d\n", *rule_ptr);
